6514ff2049718fe99cac7b73b8cf235ca7e19905
HR Admin <hr@bsctextiles.com>
2026-08-10 14:25:30 +0530

    build: add code-split production bundle for performance optimization

---
+++ hrms-system/client/dist/assets/Home-oRnP148q.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-wkT3-BaF.js";import{N as o}from"./react-vendor-Dax-Hste.js";import"./lucide-icons-D1tzUE21.js";function i(){return t.jsx(o,{to:"/login",replace:!0})}export{i as default};
---
+++ hrms-system/client/dist/assets/formatName-B_WQHSH-.js
@@ -0,0 +1,1 @@
+function t(e){return e?e.toLowerCase().replace(/\b\w/g,r=>r.toUpperCase()):""}export{t as f};
