083b530a38bcaabc1fd760cba5bb43cd19bc47c9
HR Admin <hr@bsctextiles.com>
2026-08-13 16:59:53 +0530

    style: premium card redesign with 20px radius cards and typography hierarchy upgrade for Joining Call Desk

---
+++ hrms-system/client/dist/assets/Home-DWYPoYwp.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-7Xb6yM5d.js";import{N as o}from"./react-vendor-BuYX3WMP.js";import"./lucide-icons-CZe6QT-o.js";function i(){return t.jsx(o,{to:"/login",replace:!0})}export{i as default};
